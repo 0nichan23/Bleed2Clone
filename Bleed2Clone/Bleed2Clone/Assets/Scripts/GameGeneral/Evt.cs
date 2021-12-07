0c7b2d96e3cd5e528844577fa9ae0e7b9db3e5ba
doverlydia <68915121+doverlydia@users.noreply.github.com>
2021-12-07 23:12:04 +0200

    Update Evt.cs

--- Bleed2Clone/Bleed2Clone/Assets/Scripts/GameGeneral/Evt.cs
+++ Bleed2Clone/Bleed2Clone/Assets/Scripts/GameGeneral/Evt.cs
@@ -3,30 +3,30 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-public static class Evt
-{
-    public static readonly Evt GameStarted = new Evt();
-    public static readonly Evt GamePaused = new Evt();
-    public static readonly Evt GameEnded = new Evt();
-}
+//public static class Evt
+//{
+//    public static readonly Evt GameStarted = new Evt();
+//    public static readonly Evt GamePaused = new Evt();
+//    public static readonly Evt GameEnded = new Evt();
+//}
 
-public class Evt
-{
-    public event Action Action = delegate { };
+//public static class Evt
+//{
+//    public static event Action Action = delegate { };
 
-    public void Invoke()
-    {
-        Action.Invoke(enemy);
-    }
+//    public void Invoke()
+//    {
+//        Action.Invoke(enemy);
+//    }
 
-    public void UpsertListener(Action<Enemy> listener)
-    {
-        Action -= listener;
-        Action += listener;
-    }
+//    public void UpsertListener(Action<Enemy> listener)
+//    {
+//        Action -= listener;
+//        Action += listener;
+//    }
 
-    public void RemoveListener(Action<Enemy> listener)
-    {
-        Action -= listener;
-    }
-}
+//    public void RemoveListener(Action<Enemy> listener)
+//    {
+//        Action -= listener;
+//    }
+//}
