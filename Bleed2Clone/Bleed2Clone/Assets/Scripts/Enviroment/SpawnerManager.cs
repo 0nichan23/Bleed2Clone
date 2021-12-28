a3db541369e335f3833e894245e0179b028c044a
Greenburger <greenburger@gmail.com>
2021-12-28 21:45:09 +0200

    Added enemy spawner

---
+++ Bleed2Clone/Bleed2Clone/Assets/Scripts/Enviroment/SpawnerManager.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpawnerManager : MonoBehaviour {
+
+	public GameObject enemy;
+	public int maxNumberOfSpawns;
+	private int currentNumberOfSpawns;
+	public float timeBetweenSpawning;
+	public float timeOfSpawn;
+
+	// Use this for initialization
+	void Start () {
+		currentNumberOfSpawns = maxNumberOfSpawns;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (currentNumberOfSpawns > 0) {
+			if (Time.time - timeOfSpawn >= timeBetweenSpawning) {
+				Instantiate (enemy, transform.position, transform.rotation);
+				timeOfSpawn = Time.time;
+				currentNumberOfSpawns -= 1;
+			}
+		}
+	}
+}
