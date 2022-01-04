34f5a252f3bc99ba5b100b1f53bdd5ee241b07db
Greenburger <greenburger@gmail.com>
2022-01-04 20:52:50 +0200

    Fixed platform script

--- Bleed2Clone/Bleed2Clone/Assets/Scripts/Enviroment/PlatformBehavior.cs
+++ Bleed2Clone/Bleed2Clone/Assets/Scripts/Enviroment/PlatformBehavior.cs
@@ -6,35 +6,57 @@ public class PlatformBehavior : MonoBehaviour
 {
 
     private PlatformEffector2D effector;
+    public BoxCollider2D platform;
 
     public float waitTime;
-    // Start is called before the first frame update
-    void Start()
-    {
-        effector = GetComponent<PlatformEffector2D>();
-    }
 
     // Update is called once per frame
     void Update()
     {
         if (Input.GetKeyUp(KeyCode.DownArrow))
-            waitTime = 0.5f;
+        {
+            waitTime = 0.2f;
+            platform.enabled = true;
+        }
 
         if (Input.GetKey(KeyCode.DownArrow))
         {
             if (waitTime <= 0)
             {
-                effector.rotationalOffset = 180;
-                waitTime = 0.5f;
+                //effector.rotationalOffset = 180;
+                waitTime = 0.2f;
+                platform.enabled = false;
             }
             else waitTime -= Time.deltaTime;
-
-            //effector.surfaceArc = 0;
         }
 
+        //effector.surfaceArc = 0;
         if (Input.GetKey(KeyCode.UpArrow))
         {
-            effector.rotationalOffset = 0;
+            //platform.isTrigger = false;
+            //effector.rotationalOffset = 0;
+        }
+
+        //if (Input.GetKey(KeyCode.UpArrow))
+        void OnCollisionEnter2D(Collision2D collision)
+        {
+            if (collision.gameObject.name == "Player")
+            {
+                Debug.Log("enter");
+                collision.gameObject.transform.SetParent(transform);
+            }
+        }
+
+        void OnCollisionExit2D(Collision2D collision)
+        {
+            if (collision.gameObject.name == "Player")
+            {
+                effector.rotationalOffset = 0;
+                Debug.Log("exit");
+                collision.gameObject.transform.SetParent(null);
+                platform.enabled = true;
+            }
         }
     }
 }
+
