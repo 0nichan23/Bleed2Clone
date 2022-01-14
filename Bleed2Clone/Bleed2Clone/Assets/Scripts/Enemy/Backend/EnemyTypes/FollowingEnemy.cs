2bc223ddfb6a4df61154defdc32e42f525defc29
Chironyx <MusicalElixir@gmail.com>
2022-01-14 20:23:27 +0200

    <

--- Bleed2Clone/Bleed2Clone/Assets/Scripts/Enemy/Backend/EnemyTypes/FollowingEnemy.cs
+++ Bleed2Clone/Bleed2Clone/Assets/Scripts/Enemy/Backend/EnemyTypes/FollowingEnemy.cs
@@ -17,7 +17,7 @@ public class FollowingEnemy : EnemyDatabase
         while (true)
         {
             enemy.weapon.Shoot();
-            if (Vector2.Distance(enemy.transform.position, enemy.player.position) > 1)
+            if (Vector2.Distance(enemy.transform.position, enemy.player.position) < 1)
             {
                 float diff = (enemy.transform.position.x - enemy.player.position.x);
                 enemy.agent.SetDestination(new Vector2(enemy.player.position.x + 3 * Mathf.Sign(diff), enemy.transform.position.y));
