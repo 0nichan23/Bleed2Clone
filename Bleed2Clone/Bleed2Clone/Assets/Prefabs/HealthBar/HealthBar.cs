b8315c65e22174ddd6fe8ca0077bba75b6b371e6
doverlydia <68915121+doverlydia@users.noreply.github.com>
2022-01-16 15:12:06 +0200

    Health Bars

---
+++ Bleed2Clone/Bleed2Clone/Assets/Prefabs/HealthBar/HealthBar.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthBar : MonoBehaviour
+{
+    Slider healthBarSlider;
+    Damagable damagable;
+    private void Start()
+    {
+        healthBarSlider = transform.GetChild(0).GetComponentInChildren<Slider>();
+        damagable= GetComponentInParent<Damagable>();
+        healthBarSlider.maxValue = damagable.GetMaxHP();
+
+    }
+    private void Update()
+    {
+        healthBarSlider.value = damagable.GetCurrentHP();
+    }
+
+}
