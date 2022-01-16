1126b382c90059b003d0c0af074fa7345b4e8d4f
doverlydia <68915121+doverlydia@users.noreply.github.com>
2022-01-16 06:00:15 +0200

    music

---
+++ Bleed2Clone/Bleed2Clone/Assets/AudioManager.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    [SerializeField] AudioSource audioSource;
+    [SerializeField] AudioClip intro;
+    [SerializeField] AudioClip gameLoop;
+
+    private void Start()
+    {
+        StartCoroutine(PlayGameMusic());
+    }
+
+    IEnumerator PlayGameMusic()
+    {
+        audioSource.clip = intro;
+        audioSource.Play();
+        yield return new WaitForSeconds(intro.length);
+        audioSource.clip = gameLoop;
+        audioSource.loop = true;
+        audioSource.Play();
+    }
+}
