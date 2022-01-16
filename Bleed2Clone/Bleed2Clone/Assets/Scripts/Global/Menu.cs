7affe1c73af5ede458b23349b0b3252053f7b201
doverlydia <68915121+doverlydia@users.noreply.github.com>
2022-01-16 06:27:04 +0200

    Menu

--- Bleed2Clone/Bleed2Clone/Assets/Scripts/Global/Menu.cs
+++ Bleed2Clone/Bleed2Clone/Assets/Scripts/Global/Menu.cs
@@ -4,9 +4,14 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 public class Menu : MonoBehaviour
 {
-   public void LoadScene(int sceneIndex)
+    public void LoadScene(int sceneIndex)
     {
         Time.timeScale = 1;
         SceneManager.LoadScene(sceneIndex);
+    }
+
+    public void ToggleActive(GameObject gameObject)
+    {
+        gameObject.SetActive(!gameObject.activeSelf);
     }
 }
