d725f5051355962d7bed9bc147aa1d55e04d8cc5
Chironyx <MusicalElixir@gmail.com>
2022-01-16 09:13:47 +0200

    Outlines fixed + camera prefab + Correct player turn and squash & stretch

--- Bleed2Clone/Bleed2Clone/Assets/Scripts/Effects/PlayerAnimator.cs
+++ Bleed2Clone/Bleed2Clone/Assets/Scripts/Effects/PlayerAnimator.cs
@@ -5,13 +5,16 @@ using UnityEngine;
 public class PlayerAnimator : MonoBehaviour
 {
     [SerializeField] private Rigidbody2D rb2d;
-    [SerializeField] private SpriteRenderer SR;
+    [SerializeField] private SpriteRenderer[] SR;
+    [SerializeField] private Transform gfxParent;
     [SerializeField] private Animator anim;
+    private float flippedSign;
     private bool isGrounded;
 
     private void Start()
     {
         GameEvents.OnPlayerGroundedEvent += UpdateIsGrounded;
+        flippedSign = 1f;
     }
 
     private void OnDisable()
@@ -22,14 +25,26 @@ public class PlayerAnimator : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        bool changedFlip = false;
         float horiVelocity = rb2d.velocity.x;
         float vertiVelocity = rb2d.velocity.y;
         anim.SetFloat("HoriSpeed", Mathf.Abs(horiVelocity));
         anim.SetFloat("VertiSpeed", vertiVelocity);
-        if (horiVelocity < -0.5f && !SR.flipX)
-            SR.flipX = true;
-        else if (horiVelocity > 0.5f && SR.flipX)
-            SR.flipX = false;
+        if (horiVelocity < -0.5f && flippedSign == 1)
+        {
+            flippedSign = -1;
+            changedFlip = true;
+        }
+        else if (horiVelocity > 0.5f && flippedSign == -1)
+        {
+            flippedSign = 1;
+            changedFlip = true;
+        }
+
+        if (changedFlip)
+        {
+            gfxParent.localScale = new Vector3(flippedSign, 1, 1);
+        }
     }
 
     private void UpdateIsGrounded(bool grounded)
@@ -37,8 +52,8 @@ public class PlayerAnimator : MonoBehaviour
         anim.SetBool("IsGrounded", grounded);
         isGrounded = grounded;
 
-        if (grounded) SquashAndStretch(new Vector2(1.2f, 0.8f), 10f, true);
-        else SquashAndStretch(new Vector2(0.9f, 1.1f), 2f, true);
+        if (grounded) SquashAndStretch(new Vector2(1.3f, 0.7f), 10f, true);
+        else SquashAndStretch(new Vector2(0.8f, 1.2f), 2f, true);
     }
 
     private void SquashAndStretch(Vector2 amount, float rate, bool shouldReturn)
@@ -48,14 +63,15 @@ public class PlayerAnimator : MonoBehaviour
 
     private IEnumerator DoSquashAndStretch(Vector2 amount, float rate, bool shouldReturn)
     {
-        Transform srTrans = SR.transform;
+        Transform srTrans = gfxParent;
+        float currentFlippedSign = flippedSign;
 
         float lerpAmount = 0f;
         while(lerpAmount < 1f)
         {
             lerpAmount += rate * Time.deltaTime;
             Vector3 scale = srTrans.localScale;
-            scale.x = Mathf.Lerp(1f, amount.x, lerpAmount);
+            scale.x = Mathf.Lerp(flippedSign, amount.x * flippedSign, lerpAmount);
             scale.y = Mathf.Lerp(1f, amount.y, lerpAmount);
             srTrans.localScale = scale;
             yield return null;
@@ -68,7 +84,7 @@ public class PlayerAnimator : MonoBehaviour
             {
                 lerpAmount += rate * Time.deltaTime;
                 Vector3 scale = srTrans.localScale;
-                scale.x = Mathf.Lerp(amount.x, 1f, lerpAmount);
+                scale.x = Mathf.Lerp(amount.x * flippedSign, flippedSign, lerpAmount);
                 scale.y = Mathf.Lerp(amount.y, 1f, lerpAmount);
                 srTrans.localScale = scale;
                 yield return null;
